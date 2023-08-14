97955c87de0bab43176063ecf247c7d8ad894baa
Tiago Seabra <tlgs@users.noreply.github.com>
2023-08-14 02:33:19 +0100

    Add initial Parser code
    This is the code from Chapter 6: Parsing Expressions.

--- com/jlox/lox/Lox.java
+++ com/jlox/lox/Lox.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-    static boolean hadError = false;
+  static boolean hadError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -46,11 +46,13 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    // Stop if there was a syntax error.
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
   }
 
   static void error(int line, String message) {
@@ -62,4 +64,11 @@ public class Lox {
     hadError = true;
   }
 
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
 }
