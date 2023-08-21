aa679e50368a4395f4ad93e3be0286f27c18cf4d
Tiago Seabra <tlgs@users.noreply.github.com>
2023-08-22 00:31:14 +0100

    Add functions
    This is the code from Chapter 10: Functions.

--- misc/generate_ast.py
+++ misc/generate_ast.py
@@ -87,6 +87,7 @@ def main() -> int:
     expr_types = [
         ("Assign", "Token name, Expr value"),
         ("Binary", "Expr left, Token operator, Expr right"),
+        ("Call", "Expr callee, Token paren, List<Expr> arguments"),
         ("Grouping", "Expr expression"),
         ("Literal", "Object value"),
         ("Logical", "Expr left, Token operator, Expr right"),
@@ -98,8 +99,10 @@ def main() -> int:
     stmt_types = [
         ("Block", "List<Stmt> statements"),
         ("Expression", "Expr expression"),
+        ("Function", "Token name, List<Token> params, List<Stmt> body"),
         ("If", "Expr condition, Stmt thenBranch, Stmt elseBranch"),
         ("Print", "Expr expression"),
+        ("Return", "Token keyword, Expr value"),
         ("Var", "Token name, Expr initializer"),
         ("While", "Expr condition, Stmt body"),
     ]
